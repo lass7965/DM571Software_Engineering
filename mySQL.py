73a3d6f5b8b71fef5d4bbf1b2abd474bd2351d94
Lasse Sørensen <lsoer17@student.sdu.dk>
2019-11-20 14:48:15 +0100

    Added roster to the mySQL database + foreign keys
    With the introduction of foreign keys, I had to introduce UserID into the database.

--- mySQL.py
+++ mySQL.py
@@ -1,11 +1,14 @@
 import mysql.connector
 import datetime
-logindb = mysql.connector.connect(
-    host = "mysql23.unoeuro.com",
-    user = "lkis_dk",
-    passwd = "DM571Software",
-    database="lkis_dk_db"
-)
+try:
+    logindb = mysql.connector.connect(
+        host = "mysql23.unoeuro.com",
+        user = "lkis_dk",
+        passwd = "DM571Software",
+        database="lkis_dk_db"
+    )
+except:
+    print("Connection to the mySQL failed!")
 
 def getFullTable():
     cursor = logindb.cursor()
@@ -14,50 +17,69 @@ def getFullTable():
     cursor.close()
     return ret
 
-def getPasswd(username):
+def getUUID(username):
     cursor = logindb.cursor()
-    cursor.execute("SELECT t1.password FROM Login AS t1 WHERE t1.username = '%s';" % username)
+    cursor.execute("SELECT User.UserID FROM User WHERE User.Username = '%s';" % username)
+    ret = cursor.fetchone()[0].decode()
+    cursor.close()
+    return ret
+
+def getPasswd(username):
+    UUID = getUUID(username)
+    cursor = logindb.cursor()
+    cursor.execute("SELECT t1.password FROM Login AS t1 WHERE t1.UserID = '%s';" % UUID)
     ret = cursor.fetchone()[0]
     cursor.close()
     return ret
 
 #Perhaps a more secure login, as the real password never enters the machine, and is handled by the SQL server?
 def checkPasswd(username,passwd):
+    UUID = getUUID(username)
+    cursor = logindb.cursor()
+    cursor.execute("SELECT t1.UserID FROM Login AS t1 WHERE t1.UserID = '%s' AND t1.Password = '%s';" % (UUID, passwd))
     try:
-        cursor = logindb.cursor()
-        cursor.execute("SELECT t1.username FROM Login AS t1 WHERE t1.username = '%s' AND t1.password = '%s';" % (username,passwd))
-        ret = cursor.fetchone()[0]
+        cursor.fetchone()[0] #This command fails if user and password does not match.
         cursor.close()
         return True
     except:
+        cursor.close()
         return False
 
 def updateLastLogin(username):
+    UUID = getUUID(username)
     cursor = logindb.cursor()
     today = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    cursor.execute("UPDATE Login SET Last_Login = '"+today+"' WHERE Login.username = '%s'" % username)
+    cursor.execute("UPDATE Login SET Last_Login = '"+today+"' WHERE Login.UserID = '%s'" % UUID)
     logindb.commit()
     cursor.close()
 
 def getLastLogin(username):
+    UUID = getUUID(username)
     cursor = logindb.cursor()
-    cursor.execute("SELECT t1.Last_Login FROM Login AS t1 WHERE t1.username = '%s';" % username)
-    return cursor.fetchone()[0]
+    cursor.execute("SELECT t1.Last_Login FROM Login AS t1 WHERE t1.UserID = '%s';" % UUID)
+    ret = cursor.fetchone()[0]
     cursor.close()
+    return ret
 
 def createUser(username, password, email, permission):
+    cursor = logindb.cursor()
     try:
-        cursor = logindb.cursor()
         today = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        query = "INSERT INTO Login VALUES ('%s','%s','%s',%d,'%s','%s');" %(username, password, email, permission,today,today)
-        cursor.execute(query, )
+        query = "INSERT INTO User (UserID, Username, Email, Permission) VALUES(UUID(), '%s','%s',%d);" %(username, email, permission)
+        cursor.execute(query)
+        UUID = getUUID(username)
+        query = "INSERT INTO Login VALUES('%s','%s','%s','%s');" % (UUID, password, today, today)
+        cursor.execute(query)
         logindb.commit()
         cursor.close()
     except:
         print("User or email is already used")
+        cursor.close()
 
 def getPermissionLevel(username):
+    UUID = getUUID(username)
     cursor = logindb.cursor()
-    cursor.execute("SELECT t1.Permission FROM Login AS t1 WHERE t1.username = '%s';" % username)
-    return cursor.fetchone()[0]
+    cursor.execute("SELECT t1.Permission FROM Login AS t1 WHERE t1.UserID = '%s';" % UUID)
+    ret = cursor.fetchone()[0]
     cursor.close()
+    return ret
